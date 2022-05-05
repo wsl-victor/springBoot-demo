3f328ab9b0028454b9488c80980bf899e8cd4185
apple <abc>
2022-05-05 20:31:56 +0800

    init

---
+++ springboot-druid-mysql-demo/src/main/java/com/wsl/druid/SpringbootDruidMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wsl.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDruidMysqlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDruidMysqlDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-druid-mysql-demo/src/test/java/com/wsl/druid/SpringbootDruidMysqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsl.druid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDruidMysqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
